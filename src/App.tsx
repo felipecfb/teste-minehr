94bb1536e5f09bc704a386253f6088a1edacbbaa
Felipe Bastos <felipe.bastos@dt3.com.br>
2022-06-10 10:02:03 -0300

    feat: criando componente Topbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import React from "react";
+import Topbar from "./components/Topbar";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world</h1>
-    </div>
+    <Topbar />
   );
 }
 
---
+++ src/components/Topbar/styles.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    background: var(--light-purple);
+    min-height: 10vh;
+    min-width: 100vw;
+`
