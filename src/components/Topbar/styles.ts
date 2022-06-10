6d59762f661a9320167031683326cd3f90d0db32
Felipe Bastos <felipe.bastos@dt3.com.br>
2022-06-10 10:27:22 -0300

    style: alterando width do topbar de 100vw para 100%

--- src/components/Topbar/styles.ts
+++ src/components/Topbar/styles.ts
@@ -3,5 +3,5 @@ import styled from 'styled-components';
 export const Container = styled.div`
     background: var(--light-purple);
     min-height: 10vh;
-    min-width: 100vw;
+    min-width: 100%;
 `
