98c370dc075f156834865207be089099ce5c5acf
Felipe Bastos | DT3 <felipe.bastos@dt3.com.br>
2022-06-14 10:05:53 -0300

    style: ajustando header para width 100%

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -5,6 +5,7 @@ export const Container = styled.div`
     display: flex;
     justify-content: space-between;
     align-items: center;
+    width: 100%;
 `;
 
 export const TextContainer = styled.div`
